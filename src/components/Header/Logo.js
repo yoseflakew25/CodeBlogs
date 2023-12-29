e53f53aa0ce27fd09e7045da5fd398b9191de4ec
yosef lakew <80267669+yoseflakew25@users.noreply.github.com>
2023-12-29 13:40:05 +0300

    Update Logo.js

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -5,10 +5,8 @@ import profileImg from "../../../public/logo.png"
 const Logo = () => {
   return (
     <Link href="/" className="flex items-center text-dark dark:text-light">
-        <div className=" w-12 md:w-16 rounded-full overflow-hidden border border-solid border-dark dark:border-gray  mr-2 md:mr-4">
-            <Image src={profileImg} alt="CodeBucks logo" className="w-full h-auto rounded-full" sizes="20vw" priority />
-        </div>
-        <span className="font-bold dark:font-semibold text-lg md:text-xl">DevBlogs</span>
+
+        <span className="font-bold dark:font-semibold text-lg md:text-xl">Hi, I'm Yosef Lakew 👋</span>
     </Link>
   )
 }
